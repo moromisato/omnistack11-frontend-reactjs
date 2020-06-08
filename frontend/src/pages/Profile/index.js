79a62efea08c8b5779c50730f353db91173051b7
Luis Moromisato <moromisato.luis@gmail.com>
2020-06-08 12:19:50 -0300

    added handleDeleteIncident function added function to format number in brl currency

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg'
 
@@ -10,6 +10,7 @@ import './styles.css'
 export default function Profile() {
 
     const [incidents, setIncidents] = useState([])
+    const history = useHistory()
     const ongId = localStorage.getItem('ongId')
     const ongName = localStorage.getItem('ongName')
 
@@ -24,13 +25,42 @@ export default function Profile() {
         })
     }, [ongId])
 
+    async function handleDeleteIncident(id) {
+        try {
+            console.log('clicked')
+            await api.delete(`incidents/${id}`, {
+                headers: {
+                    Authorization: ongId,
+                }
+            });
+
+            setIncidents(incidents.filter(incident => incident.id !== id));
+
+        } catch (error) {
+            alert('Erro ao deletar incidente')
+        }
+
+    }
+
+    function handleLogout() {
+        localStorage.clear()
+        history.push('/')
+    }
+
+    function numberFormat(value) {
+        return new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(value);
+    }
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="heroes"/>
                 <span>Bem vinda, {ongName}</span>
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={() => handleLogout()} type="button">
                     <FiPower size={18} color="#e02041"/>
                 </button>
             </header>
@@ -44,8 +74,8 @@ export default function Profile() {
                     <strong>Descrição:</strong>
                     <p>{incident.description}</p>
                     <strong>Valor:</strong>
-                    <p>{Intl.NumberFormat('pt-BR', {style: 'currancy', currency: 'BRL'}).format(incident.value)}</p>
-                    <button type="button">
+                    <p>{numberFormat(incident.value)}</p>
+                    <button onClick={() => handleDeleteIncident(incident.id)} type="button">
                         <FiTrash2 size={20} color="#a8a8b3" />
                     </button>
                     </li>
